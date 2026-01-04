02ee538f07d8069d65bd89a1073aaad6efc30a24
ryanhenrqq <ryanh8116@gmail.com>
2026-01-04 16:06:18 -0300

    feat/style: added some effects into header picture and button

--- src/script/script.js
+++ src/script/script.js
@@ -1,19 +1,26 @@
 let headerImgStatus = 0 // 0: logo, 1: foto
 function changeHeaderImage() {
     const element = document.getElementById("image-head")
-    if (headerImgStatus == 1) {
-        const srcSet = "./src/res/logo/logo-image.png"
-        element.src = srcSet
-        headerImgStatus = 0
-    } else if (headerImgStatus == 0) {
-        const srcSet = "./src/res/logo/my-photo.jpg"
-        element.src = srcSet
-        headerImgStatus = 1
-    } else {
-        console.error("Erro na troca automatica da foto, retornando ao padrão")
-        const srcSet = "./src/res/logo/logo-image.png"
-        element.src = srcSet
-    }
+    element.style.opacity = 0
+
+    setTimeout(() => {
+        
+        if (headerImgStatus == 1) {
+            const srcSet = "./src/res/logo/logo-image.png"
+            element.src = srcSet
+            headerImgStatus = 0
+        } else if (headerImgStatus == 0) {
+            const srcSet = "./src/res/logo/my-photo.jpg"
+            element.src = srcSet
+            headerImgStatus = 1
+        } else {
+            console.error("Erro na troca automatica da foto, retornando ao padrão")
+            const srcSet = "./src/res/logo/logo-image.png"
+            element.src = srcSet
+        }
+        element.style.opacity = 1
+    }, 400)
+
 }
 
 setInterval(changeHeaderImage, 3000)
