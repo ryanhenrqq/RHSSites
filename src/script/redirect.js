f2f34a3ecb833718df347c24e0dcf479d4920273
ryanhenrqq <ryanh8116@gmail.com>
2026-01-06 21:51:11 -0300

    feat: added a new blank contact page

--- src/script/redirect.js
+++ src/script/redirect.js
@@ -4,11 +4,14 @@ function goToGithub() {
 function goToPortfolio() {
     window.location.replace("./pg/portfolio.html")
 }
+function goContact() {
+    window.location.replace("./pg/contact.html")
+}
 function goToEmail() {
     window.open("mailto:rhs.sites.8116@gmail.com")
 }
 
-// go back to home (pagina do portfolio)
+// go back to home (paginas dentro de pg)
 function goHome() {
     window.location.replace("../index.html")
 }
