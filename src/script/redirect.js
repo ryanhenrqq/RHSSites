ea2546c4b995d05670abcc6cebccf7f64b99bb41
ryanhenrqq <ryanh8116@gmail.com>
2026-01-07 22:08:24 -0300

    feat: portfolio: added 3 personal projects to the page & chore: created a 404 error page

--- src/script/redirect.js
+++ src/script/redirect.js
@@ -11,7 +11,42 @@ function goToEmail() {
     window.open("mailto:rhs.sites.8116@gmail.com")
 }
 
+// Portfolio
+function goToFloodTheCookie(index) {
+    if (index === 0) {
+        window.open("https://ryanhenrqq.github.io/FloodTheCookie/")
+    } else if (index === 1) {
+        window.open("https://www.github.com/ryanhenrqq/FloodTheCookie/")
+    } else {
+        goTo404()
+    }
+}
+
+function goToOoniquePass(index) {
+    if (index === 0) {
+        window.open("https://ryanhenrqq.github.io/OoniquePass/")
+    } else if (index === 1) {
+        window.open("https://www.github.com/ryanhenrqq/OoniquePass/")
+    } else {
+        goTo404()
+    }
+}
+
+function goToToDoTasks(index) {
+    if (index === 0) {
+        window.open("https://ryanhenrqq.github.io/ToDoTasks/")
+    } else if (index === 1) {
+        window.open("https://www.github.com/ryanhenrqq/ToDoTasks/")
+    } else {
+        goTo404()
+    }
+}
+
 // go back to home (paginas dentro de pg)
 function goHome() {
     window.location.replace("../index.html")
+}
+
+function goTo404() {
+    window.location.replace("./except/404.html")
 }
