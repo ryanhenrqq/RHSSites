2098b7ade5daaa05e9d977fc4a18d6609059448f
ryanhenrqq <ryanh8116@gmail.com>
2026-01-23 23:57:42 -0300

    feat: added formspree backend for form, but not configured yet

--- src/script/contact.js
+++ src/script/contact.js
@@ -1,4 +1,5 @@
 const formRoot = document.getElementById("contact-email-form")
+const formRpo = document.getElementById("contact-report-form")
 
 // Aviso de formulario desativado temporario
 formRoot.addEventListener("submit", async (e) => {
@@ -7,6 +8,31 @@ formRoot.addEventListener("submit", async (e) => {
 
     try {
         const response = await fetch("https://formspree.io/f/mbddjrwd", {
+            method: "POST",
+            body: formData,
+            headers: {
+                "Accept": "application/json"
+            }
+        })
+
+        if (response.ok) {
+            window.location.replace("./submited.html")
+        } else {
+            console.error("Erro no envio do formulario: Dentro da condicional do submit")
+            setTimeout(goTo500, 5000)
+        }
+    } catch(err) {
+        console.error(err)
+        setTimeout(goTo500, 5000)
+    }
+})
+
+formRpo.addEventListener("submit", async (e) => {
+    e.preventDefault()
+    const formData = new FormData(formRpo)
+
+    try {
+        const response = await fetch("https://formspree.io/f/mpqpyykr", {
             method: "POST",
             body: formData,
             headers: {
