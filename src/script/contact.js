281bdea222f1086477a61d6eec13b202d555d9ce
ryanhenrqq <ryanh8116@gmail.com>
2026-02-04 22:15:02 -0300

    fix: report form is now working as well

--- src/script/contact.js
+++ src/script/contact.js
@@ -1,59 +1,66 @@
 const formRoot = document.getElementById("contact-email-form")
 const formRpo = document.getElementById("contact-report-form")
 
-// Aviso de formulario desativado temporario
-formRoot.addEventListener("submit", async (e) => {
-    e.preventDefault()
-    const formData = new FormData(formRoot)
-
-    try {
-        const response = await fetch("https://formspree.io/f/mbddjrwd", {
-            method: "POST",
-            body: formData,
-            headers: {
-                "Accept": "application/json"
-            }
-        })
-
-        if (response.ok) {
-            changeToSubmited()
-        } else {
-            console.error("Erro no envio do formulario: Dentro da condicional do submit")
-            setTimeout(goTo500, 5000)
-        }
-    } catch(err) {
-        console.error(err)
-        setTimeout(goTo500, 5000)
-    }
-})
 function changeToSubmited() {
     const subElem = document.getElementById("submitted")
     const frontElem = document.getElementById("form-frontpanel")
     subElem.style.display = "flex"
     frontElem.style.display = "none"
 }
-// necessario corrigir bug - envio de formulario incorreto - formRoot is null 5:1
-formRpo.addEventListener("submit", async (e) => {
-    e.preventDefault()
-    const formDatar = new FormData(formRpo)
 
-    try {
-        const response = await fetch("https://formspree.io/f/mpqpyykr", {
-            method: "POST",
-            body: formDatar,
-            headers: {
-                "Accept": "application/json"
+if (formRoot) {
+    // Aviso de formulario desativado temporario
+    formRoot.addEventListener("submit", async (e) => {
+        e.preventDefault()
+        const formData = new FormData(formRoot)
+
+        try {
+            const response = await fetch("https://formspree.io/f/mbddjrwd", {
+                method: "POST",
+                body: formData,
+                headers: {
+                    "Accept": "application/json"
+                }
+            })
+
+            if (response.ok) {
+                changeToSubmited()
+            } else {
+                console.error("Erro no envio do formulario: Dentro da condicional do submit")
+                setTimeout(goTo500, 5000)
             }
-        })
-
-        if (response.ok) {
-            window.location.replace("./submited.html")
-        } else {
-            console.error("Erro no envio do formulario: Dentro da condicional do submit")
+        } catch(err) {
+            console.error(err)
             setTimeout(goTo500, 5000)
         }
-    } catch(err) {
-        console.error(err)
-        setTimeout(goTo500, 5000)
-    }
-})
+    })
+
+}
+
+if (formRpo) {
+    // necessario corrigir bug - envio de formulario incorreto - formRoot is null 5:1
+    formRpo.addEventListener("submit", async (e) => {
+        e.preventDefault()
+        const formData = new FormData(formRpo)
+
+        try {
+            const response = await fetch("https://formspree.io/f/mpqpyykr", {
+                method: "POST",
+                body: formData,
+                headers: {
+                    "Accept": "application/json"
+                }
+            })
+
+            if (response.ok) {
+                changeToSubmited()
+            } else {
+                console.error("Erro no envio do formulario: Dentro da condicional do submit")
+                setTimeout(goTo500, 5000)
+            }
+        } catch(err) {
+            console.error(err)
+            setTimeout(goTo500, 5000)
+        }
+    })
+}
